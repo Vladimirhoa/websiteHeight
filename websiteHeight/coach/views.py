8c8dab8b9771acc69829571c501285f8266a341c
Vladimirhoa <rozhdestvo.vi@mail.ru>
2025-12-01 00:54:36 +0300

    Настроил новый стиль сайта

--- websiteHeight/coach/views.py
+++ websiteHeight/coach/views.py
@@ -1,23 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import Coach
 
 def coach_detail(request):
-    # 1. Получаем первого (или единственного) тренера по ID 1.
-    # get_object_or_404 автоматически вызовет ошибку 404,
-    # если тренера с ID=1 нет в базе (лучше, чем ошибка сервера).
-    try:
-        coach = Coach.objects.get(pk=1)
-    except Coach.DoesNotExist:
-        # Если вы хотите более гибкий подход (например, если нет тренера с pk=1),
-        # можно просто взять первого попавшегося, если он есть
-        coach = Coach.objects.first()
-        if not coach:
-            # Если в базе вообще нет тренеров
-            return render(request, 'coach/coach_detail.html', {'coach': None})
+    # Получаем ВСЕХ тренеров из базы данных
+    coaches = Coach.objects.all()
 
     context = {
-        'coach': coach,
-        'page_title':'Наши Тренеры'
+        'coaches': coaches,
+        'page_title': 'Наши Тренеры'
     }
-    # 2. Передаем данные в шаблон
+    # Используем тот же шаблон (или можно переименовать его в coach_list.html)
     return render(request, 'coach/coach_detail.html', context)
