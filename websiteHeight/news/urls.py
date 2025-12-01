179dea10388b3ef2e4da117cc7f1479ef298e9e1
Vladimirhoa <rozhdestvo.vi@mail.ru>
2025-12-01 15:17:32 +0300

    Лайки и комментарии

--- websiteHeight/news/urls.py
+++ websiteHeight/news/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('<int:pk>/', views.news_details, name='news_page'),
+    # Новый маршрут для лайка
+    path('<int:pk>/like/', views.news_like, name='news_like'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
 ]
